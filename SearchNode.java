99aa09622e8181490faa0ed56fb1cd6e5a72f175
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2011-02-24 10:53:33 -0500

    added java files

---
+++ SearchNode.java
@@ -0,0 +1,160 @@
+/*Zeeshan Lakhani HW Networking/Search Node --> 11/23/2010 */
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class SearchNode {
+	
+	private SearchNode parent; 
+	private String address;
+	private int cum_cost;
+	
+	
+	public SearchNode (SearchNode p, String a, int c) {
+		parent = p;
+		address = a;
+		cum_cost = c;	
+	}
+	
+	public static void main(String args[]) {
+		Console console = System.console();
+		String init = console.readLine("Init Value:").toUpperCase();
+		String goal = console.readLine("Goal Value:").toUpperCase();
+		
+		File allfile = new File("random_net.txt"); 
+		ArrayList<String> allthings = new ArrayList<String>();
+		try {
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(new FileInputStream(allfile))); 
+		String line = "";
+	   	while ((line = in.readLine() ) != null) {
+				String first = line.substring(0,2);		
+				String second = line.substring(3,5);
+				allthings.add(first);
+				allthings.add(second);
+	  		} // end loop
+	   	in.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);	
+		}
+		
+		HashSet<String> hash = new HashSet<String>(allthings);
+		ArrayList<String> allvals = new ArrayList<String>(hash) ;
+		
+		Random generator = new Random();
+		int rnd_num_1 = generator.nextInt(allvals.size());
+		
+		// String init = allvals.get(rnd_num_1);
+		// allvals.remove(rnd_num_1);
+		// int rnd_num_2 = generator.nextInt(allvals.size());
+		// String goal = allvals.get(rnd_num_2);
+		// 
+				
+		// String init = "F3";
+		// String goal = "D4";
+		System.out.println("init: " + init);
+		System.out.println("goal: " + goal);
+		SearchNode final_node = search(init,goal);
+		System.out.println("last_node: " + final_node.address);
+		
+	}
+	
+	public static SearchNode search(String init, String goal) {
+		Map<String,Integer> neighborhood = new HashMap<String,Integer>();
+		ArrayList<String> done_list = new ArrayList<String>();
+		Stack<SearchNode> working_stack = new Stack<SearchNode>();
+		
+		ArrayList<String> final_path = new ArrayList<String>();
+		
+		Map<String[],Integer> network = new HashMap<String[],Integer>();
+		network = getHash();
+		
+		SearchNode current_node = new SearchNode(null,init,0);
+		//System.out.println(address);
+		
+		//etc vals
+		int add_val = 0;
+		int cum_val = 0;
+		
+		outerloop:
+		while (current_node != null) {
+			final_path.add(current_node.address);
+			neighborhood.clear();
+			Iterator eachArray = network.keySet().iterator();
+			while(eachArray.hasNext()) {
+				String[] get_array = (String[])eachArray.next();
+				if (get_array[0].equals(current_node.address))
+					neighborhood.put(get_array[1],network.get(get_array));
+				else 
+					continue;	
+			}
+			//System.out.println(neighborhood);
+			for (String s : neighborhood.keySet()) {
+				if (done_list.contains(s)) {
+					continue;
+				}
+				else {
+					add_val = neighborhood.get(s).intValue();
+					cum_val = current_node.cum_cost + add_val;
+					SearchNode new_node = new SearchNode(current_node,s,cum_val);
+					
+					if (s.equals(goal)) {
+						System.out.println("Success!");
+						
+						final_path.add(goal);
+						System.out.println("Final Path: " + final_path);
+						
+						System.out.println("Final Cost: " + cum_val);
+						break outerloop;
+					}
+					
+					else {
+						working_stack.push(new_node);
+						//System.out.println("Yes, stack it");
+						continue;
+					}	
+				}	
+			}//end for each loop through neighborhood
+			done_list.add(current_node.address);
+			//System.out.println("done_list:" + done_list);
+			current_node = working_stack.pop();	
+		}//end while loop through current node
+		
+		return current_node;
+	}//end search method
+	
+	public static HashMap<String[],Integer> getHash() {
+		File inputfile = new File("random_net.txt"); 
+		HashMap<String[],Integer> hmap = new HashMap<String[],Integer>();
+	
+		try {
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(new FileInputStream(inputfile))); 
+		String line = "";
+	   	while ((line = in.readLine() ) != null) {
+				String[] quick_array = new String[2];
+				String first = line.substring(0,2);		
+				String second = line.substring(3,5);
+				String third = line.substring(6,7);
+				
+				quick_array[0] = first;
+				quick_array[1] = second;
+				hmap.put(quick_array,Integer.parseInt(third));
+				
+				//System.out.println(first + ',' + second + ',' + third);
+				//System.out.println(hmap);
+				
+	  		} // end loop
+	   	in.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);	
+		}
+	
+		return hmap;
+		
+	}//end getHash	
+}//end SearchNode class
---
+++ WeatherMapApp.java
@@ -0,0 +1,121 @@
+//weather map app 1, incomplete
+//write necessary import statements
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class WeatherMapApp {
+	static List<String> testlist = new ArrayList<String>();
+	static String time = null;
+	//declare a static map object, weather_map
+	static Map<String,Integer> weather_map = new HashMap<String,Integer>();
+	//key set of String objects, entry set of Integer objects
+	
+	public static void main (String[] args) {
+		String base = "http://iwin.nws.noaa.gov/iwin/nj/hourly.html";
+		URL u = null;
+		URLConnection conn = null;
+		Scanner in = null;
+		try 
+		{
+			u = new URL(base);
+			conn = u.openConnection();
+			in = new Scanner(conn.getInputStream());
+			parsePage(in);
+			fillMap(testlist);
+			in.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+			
+		}
+		System.out.println("Zeeshan Lakhani LAB 5 CS 209 FAll 2010");
+		System.out.println(time);
+		//System.out.println("*Temp:-1* refers to temperatures that were labeled N/A");
+		//Note: this console output is only the testing code
+		/*for (String s:testlist)
+			System.out.println(s);*/
+			
+		//Set<String> cities = weather_map.keySet();
+		//Set<String> cities = weather_map.entrySet();
+		for (Map.Entry<String, Integer> loc : weather_map.entrySet())
+			System.out.println("City:" + loc.getKey() + " Temp:" + loc.getValue());
+		
+		try {
+	    	fileOutput();
+      		} 
+		catch (IOException e) {
+          	System.out.println("Output problem.");
+ 			}
+	} // end main
+	
+	//This function iterates over the web page, looks for --> National Weather Service, gets it's time (if null), and then finds lines under City that don't start with $$
+	//and a space. The lines must be longer than 30 in length. This line is then added to the testlist (when it finds something that contains $$ and a space or less than 30 in
+	//length, the while look breaks.
+	static void parsePage(Scanner in) { //get weather report
+		String line;
+		while (in.hasNext()) {
+			line = in.nextLine();
+			if (time == null && line.startsWith("NATIONAL WEATHER SERVICE"))
+				time = in.nextLine();
+			if (line.startsWith("CITY"))
+				while ((line = in.nextLine()) != null) {
+					if (!line.startsWith("$$") && !line.startsWith(" ") && line.length() > 30)
+						testlist.add(line);
+					else
+						break;
+				}
+			else continue;
+		}
+	} //end static parsePage
+	
+	static void fillMap(List<String> testlist) {
+		String location = "";
+		String tmp_string = ""; 
+		Integer tmp;
+		for (String s:testlist) {
+			//see instruction
+			location = s.substring(0,14);
+			tmp_string = s.substring(25,27);
+			
+			location = location.trim();
+			
+			if (location.contains("*"))
+				location = location.replace("*","");
+				
+			
+			try {
+		            Integer.parseInt(tmp_string);
+				}
+			catch (NumberFormatException ex) {
+					tmp_string = "-1";
+				}
+			
+			//System.out.println(tmp_string);
+			tmp = Integer.valueOf(tmp_string);
+			
+			if (tmp == -1)
+				continue;
+			else
+				weather_map.put(location,tmp);
+		}
+	} //end fillMap
+	
+	
+	static void fileOutput() throws IOException  { //code here
+		File outputfile = new File("city_temp.txt"); 
+	  		PrintStream out = new PrintStream(
+                new FileOutputStream(outputfile));
+			out.println("Zeeshan Lakhani LAB 5 CS 209 FAll 2010");
+			out.println(time);
+			
+	    for (Map.Entry<String, Integer> loc : weather_map.entrySet())
+			out.println("City:" + loc.getKey() + " Temp:" + loc.getValue());
+	    out.flush();
+	    out.close();
+		
+	} //end fileOutput
+	
+}//end WeatherMapApp
